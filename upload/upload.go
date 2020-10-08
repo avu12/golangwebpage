6820c0474817d74028b44c907c14404d4a5ad433
avu12 <algebra3eloadas@gmail.com>
2020-10-08 12:14:24 +0200

    detailed error messages

--- upload/upload.go
+++ upload/upload.go
@@ -12,15 +12,15 @@ func UploadHandler(c *gin.Context) {
 	file, header, err := c.Request.FormFile("upload")
 	filename := header.Filename
 	if err != nil {
-		log.Println(err)
+		log.Println("Error in form", err)
 	}
 	out, err := os.Create("uploadfiles/" + filename)
 	if err != nil {
-		log.Println(err)
+		log.Println("Error in filecreation", err)
 	}
 	defer out.Close()
 	_, err = io.Copy(out, file)
 	if err != nil {
-		log.Println(err)
+		log.Println("Error in copy data", err)
 	}
 }
