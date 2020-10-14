15fde9a0e0fba90f021fff9cb0b273da3eb10faa
avu12 <algebra3eloadas@gmail.com>
2020-10-14 23:24:54 +0200

    fix last commit

--- webpagego/server.go
+++ webpagego/server.go
@@ -24,7 +24,7 @@ var (
 func init() {
 	router = gin.Default()
 	router.SetFuncMap(template.FuncMap{
-		"kindIs": sprig.GenericFuncMap(),
+		"kindIs": sprig.GenericFuncMap,
 	})
 
 	router.LoadHTMLGlob("static/*")
